d4195f6bea513ed6cdc09a553d10d1c9ca6a4227
jayahariprasath <91586186+jayahariprasath@users.noreply.github.com>
2024-05-12 21:24:00 +0530

    Delete FRONT END INTERN directory

--- FRONT END INTERN/INTERMEDIATE/TASK 4/script.js
+++
@@ -1,6 +0,0 @@
-
-var button = document.getElementById('changeColorButton');
-button.addEventListener('click', function() {
-  var randomColor = 'rgb(' + Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256) + ')';
-  document.body.style.backgroundColor = randomColor;
-});
